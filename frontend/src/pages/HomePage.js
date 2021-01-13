46d5934a5a63ddef8d88d0be69c63fc0f67e001b
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-01-13 18:03:07 +0530

    Installed Axios, we now get product data from Node server@5000.

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,11 +1,23 @@
 // Helpers Imports
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
+import axios from "axios";
 // Component/File Imports
-import products from "../products";
 import Product from "../components/Product";
 
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+
+  // Do something after component loads
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Camera Products</h1>
