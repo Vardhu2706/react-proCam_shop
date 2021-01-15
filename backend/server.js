27785ec31e8e650de22e0272922160d5e85c20c3
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-01-15 15:58:14 +0530

    Made custom error handlers using middleware.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import colors from "colors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
@@ -15,6 +17,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
