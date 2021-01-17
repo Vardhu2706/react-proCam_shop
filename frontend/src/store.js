978beeea7fd1a428e6a5d7fc8d91441e5df03259
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-01-17 14:34:54 +0530

    Create Cart reducer & AddToCart action, implemented local storage & retrieve for cart.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,12 +5,20 @@ import {
   productDetailsReducers,
 } from "./reducers/productReducers";
 import { composeWithDevTools } from "redux-devtools-extension";
-
+import { cartReducer } from "./reducers/cartReducers";
 const reducer = combineReducers({
   productList: productListReducers,
   productDetails: productDetailsReducers,
+  cart: cartReducer,
 });
-const initialState = {};
+const carItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+const initialState = {
+  cart: {
+    cartItems: carItemsFromStorage,
+  },
+};
 const middleware = [thunk];
 const store = createStore(
   reducer,
