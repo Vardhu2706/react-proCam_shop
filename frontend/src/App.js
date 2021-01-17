a0ecc222546d74357088bc33f9f8210967404cd0
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-01-17 14:21:48 +0530

    Created Cart page and setup Routes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import CartPage from "./pages/CartPage";
 // Functional Component
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Container>
           <Route path="/" component={HomePage} exact />
           <Route path="/products/:id" component={ProductPage} />
+          <Route path="/cart/:id?" component={CartPage} />
         </Container>
       </main>
       <Footer />
